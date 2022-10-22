8711b91618f2616b5cdc671456717ee142ff0546
Rekemer <ilya.barselona@gmail.com>
2022-10-22 12:24:59 +0300

    Change name of webPage  in JavaScript

--- scripts/LoadTime.js
+++ scripts/LoadTime.js
@@ -1,7 +1,23 @@
 var start = new Date().getTime();  
 loadTime = -1;
-
+const myMap = new Map();
 window.onload = PageLoadTime;  
+myMap.set("LanguagePage.html","Почему я говорю на человеческом или как я познакомился с первый космическим языком?")
+myMap.set("Table.html","Друзья рыжего кота")
+myMap.set("","Сайт рыжего кота")
+siteName = ConvertPathToString(document.location.pathname);
+
+const pageNameContainer = document.
+    querySelector(".mainHeader").children[1];
+pageNameContainer.children[0].textContent = siteName;
+
+
+
+
+
+
+
+
 
 function PageLoadTime()
 {  
@@ -11,7 +27,15 @@ function PageLoadTime()
     querySelector("footer #loadTime");
     loadTimeContainer.textContent = "LoadTime: " + loadTime.toString()
     + " seconds";
-    console.log(document.location);
+}
+
+
+
+function ConvertPathToString(path)
+{
+    var splittedPath = path.split('/');
+    var pageName = splittedPath[splittedPath.length-1];
+    return myMap.get(pageName);
 }
 
 
